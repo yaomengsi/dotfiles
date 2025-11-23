40a68958836f508b081b4a63ebede4b95f56d870
miles <yaomengsi@outlook.com>
2025-11-23 17:53:41 +0800

    feat(dotfiles): Update zsh configuration and enhance proxy management in dotfiles

--- src/.chezmoiscripts/sh/run_onchange_install_02-starship.sh
+++ src/.chezmoiscripts/sh/run_onchange_install_02-starship.sh
@@ -4,10 +4,10 @@ set -eufo pipefail
 
 if ! command -v starship &> /dev/null; then
     echo "Installing Starship"
-    # curl -fsSL https://starship.rs/install.sh | sh -s -- -y
-    # echo "Starship installed successfully"
-    # starship preset jetpack -o ~/.config/starship.toml
-    # echo "Starship preset jetpack applied successfully"
+    curl -fsSL https://starship.rs/install.sh | sh -s -- -y
+    echo "Starship installed successfully"
+    starship preset jetpack -o ~/.config/starship.toml
+    echo "Starship preset jetpack applied successfully"
 else
     echo "Starship is already installed"
 fi
