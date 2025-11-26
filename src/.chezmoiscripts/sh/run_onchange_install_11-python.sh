7d88584905774211a0bf9fa8a06af5e2d4658076
miles <yaomengsi@outlook.com>
2025-11-26 23:14:41 +0800

    fix(dotfiles): Update Python installation script to include dependency installation for various OS distributions

--- src/.chezmoiscripts/sh/run_onchange_install_11-python.sh
+++ src/.chezmoiscripts/sh/run_onchange_install_11-python.sh
@@ -16,20 +16,29 @@ install_if_missing() {
 
 if [[ "$(uname)" == "Darwin" ]]; then
     if command -v brew &> /dev/null; then
-        echo "TODO: build python"
+        brew install openssl readline sqlite3 xz tcl-tk@8 libb2 zstd zlib pkgconfig
+        echo "build python dependencies installed successfully"
     else
         echo "Homebrew is not installed"
         exit 1
     fi
 
 elif [ -f /etc/redhat-release ]; then
-    echo "TODO: build python"
+    sudo dnf install -y make gcc patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk8-devel libffi-devel xz-devel libuuid-devel gdbm-libs libnsl2
+    sudo dnf install -y libzstd-devel
+    echo "build python dependencies installed successfully"
 
 elif [ -f /etc/debian_version ]; then
-    echo "TODO: build python"
+    sudo apt update
+    sudo apt install -y make build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev curl git \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+    sudo apt install -y libzstd-dev
+    echo "build python dependencies installed successfully"
 
 elif [ -f /etc/arch-release ]; then
-    echo "TODO: build python"
+    pacman -S --needed base-devel openssl zlib xz tk zstd
+    echo "build python dependencies installed successfully"
 
 else
     echo "Error: Unsupported OS distribution"
