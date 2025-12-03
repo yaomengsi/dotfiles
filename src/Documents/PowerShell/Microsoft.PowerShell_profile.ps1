a82393207518e25776407554e9cc434d1271f8e4
miles <yaomengsi@outlook.com>
2025-12-03 22:42:32 +0800

    feat(dotfiles): Update Windows configuration to include additional packages and add Neovide alias

--- src/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ src/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,5 @@ Invoke-Expression (&starship init powershell)
 
 function Start-mnvim { $env:NVIM_APPNAME="mnvim"; nvim }
 Set-Alias -Name mnvim -Value Start-mnvim
+function Start-nvide { $env:NVIM_APPNAME="mnvim"; neovide }
+Set-Alias -Name nvide -Value Start-nvide
