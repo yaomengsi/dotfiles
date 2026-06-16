496bc87ac71031800191f0148be01740d0e590db
miles <yaomengsi@outlook.com>
2026-06-17 07:34:51 +0800

    add pwfzf config

--- src/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ src/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,15 @@
 
 Invoke-Expression (&starship init powershell)
 
+# replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+# example command - use $Location with a different command:
+$commandOverride = [ScriptBlock]{ param($Location) Write-Host $Location }
+# pass your override to PSFzf:
+Set-PsFzfOption -AltCCommand $commandOverride
+# tab
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+
 function Start-mnvim { $env:NVIM_APPNAME="mnvim"; nvim }
 Set-Alias -Name mnvim -Value Start-mnvim
 function Start-nvide { $env:NVIM_APPNAME="mnvim"; neovide }
